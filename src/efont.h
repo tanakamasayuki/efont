8bce4b1eb79432394db4379f5d005b018dca30e9
riaosan <fiatpanda750@icloud.com>
2021-07-29 15:50:13 +0900

    for Arduino IDE

--- src/efont.h
+++ src/efont.h
@@ -10,7 +10,7 @@
 
 #define EFONT_ENABLE
 
-char* efontUFT8toUTF16( uint16_t *pUTF16, char *pUTF8 ) {
+const char *efontUFT8toUTF16(uint16_t *pUTF16, const char *pUTF8) {
 
   // 1Byte Code
   if ( pUTF8[0] < 0x80 ) {

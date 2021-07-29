aacbe333dad4b1c0f3d48b14be35519b4da6f3eb
riaosan <fiatpanda750@icloud.com>
2021-07-29 19:32:20 +0900

    modify sample

--- examples/M5StickCPlus/efontWrapper/PlatformIO/src/main.cpp
+++ examples/M5StickCPlus/efontWrapper/PlatformIO/src/main.cpp
@@ -6,10 +6,15 @@ void setup() {
     M5.begin();
     M5.Lcd.setRotation(1);  // Landscape (Portrait + 90)
     M5.Lcd.setCursor(0, 0);
-    M5.Lcd.setTextSize(2);
+    M5.Lcd.setTextSize(1);
 
-    M5.Lcd.printEfont("Hello");
-    M5.Lcd.printEfont("こんにちは");
+    M5.Lcd.printEfont("Hello", 0, 16 * 0);
+    M5.Lcd.printEfont("こんにちは", 0, 16 * 1);
+    M5.Lcd.printEfont("你好", 0, 16 * 2);
+    M5.Lcd.printEfont("안녕하세요", 0, 16 * 3);
+    M5.Lcd.printEfont("Доброе утро", 0, 16 * 4);
+    M5.Lcd.printEfont("Päivää", 0, 16 * 6);
+    M5.Lcd.printEfont("Здравствуйте", 0, 16 * 7);
 }
 
 void loop() {
